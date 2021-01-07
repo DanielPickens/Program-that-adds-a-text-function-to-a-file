a2407b3a323a74bfbc56fb58640de87e3426995f
Dan Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-01-07 12:23:16 -0700

    1st
    first coms

---
+++ maindrive.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        FileInfo finfo = new FileInfo("C:\\sri\\srip.txt");
+        using (StreamWriter writer = finfo.AppendText())
+        {
+            writer.WriteLine("New File with various Text operations");
+        }
+        finfo = new FileInfo("C:\\sri\\srip.txt");
+        using (StreamWriter writer = finfo.CreateText())
+        {
+            writer.WriteLine("New File with various Text operations");
+        }
+        using (StreamReader reader = finfo.OpenText())
+        {
+            Console.WriteLine(reader.ReadToEnd());
+        }
+        Console.Read();
+    }
+}
